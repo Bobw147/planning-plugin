20c9d9e64a3b30ad4fb7086dbf5703d6b50ee413
Bobw147 <bobw147@gmail.com>
2024-11-05 17:13:13 +0000

    Converted default status tags from string Enum to string[]

--- src/core/tags.ts
+++ src/core/tags.ts
@@ -1,23 +1,23 @@
-export enum status_tag_type {
-    STATUS_INBOX = "#status/inbox",
-    STATUS_NEXT = "#status/next",
-    STATUS_IN_PROGRESS = "#status/in-progress",
-    STATUS_WAITING_FOR = "#status/waiting-for",
-    STATUS_COMPLETED = "#status/completed",
-    STATUS_DELIVERED = "#status/delivered",
-    STATUS_ARCHIVED = "#status/archived",
-    STATUS_DEPRECATED = "#status/deprecated",
+export const status_tag_type: Array<string> = [
+    "#status/inbox",
+    "#status/next",
+    "#status/in-progress",
+    "#status/waiting-for",
+    "#status/completed",
+    "#status/delivered",
+    "#status/archived",
+    "#status/deprecated",
+]
+
+export const ident_tag_type = {
+    PLANNING_GOAL: "#planning/goal",
+    PLANNING_PROJECT: "#planning/project",
+    PLANNING_TASK: "#planning/task",
+    PLANNING_SUB_TASK: "#planning/subtask",
 }
 
-export enum ident_tag_type {
-    PLANNING_GOAL = "#planning/goal",
-    PLANNING_PROJECT = "#planning/project",
-    PLANNING_TASK = "#planning/task",
-    PLANNING_SUB_TASK = "#planning/subtask",
-}
-
-export enum mode_tag_type {
-    PLANNING_BUSINESS = "#planning/business",
-    PLANNING_PERSONAL = "#planning/personal",
-    PLANNONG_DOMESTIC = "#planning/domestic",
+export const mode_tag_type  = {
+    PLANNING_BUSINESS:" #planning/business",
+    PLANNING_PERSONAL: "#planning/personal",
+    PLANNONG_DOMESTIC: "#planning/domestic",
 }
