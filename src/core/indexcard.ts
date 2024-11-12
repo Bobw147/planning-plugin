aa7bc9611a87978e02265e52bf15980a7b12dfeb
Bobw147 <bobw147@gmail.com>
2024-11-12 17:53:23 +0000

    Now implements base class with interface and type checking

--- src/core/indexcard.ts
+++ src/core/indexcard.ts
@@ -1,12 +1,137 @@
+import { arraycopy } from "src/utils/utils";
+import { BadStatusTagError, BadModeTagError, BadIdentTagError, UserTagError } from "./exceptions/exceptions";
+export interface IIndexCard{
+    _name: string;
+    _modeTag: string;
+    _identTag: string;
+    _statusTag: string;
+    _targetDate: Date | null;
+    _anticipatedDate: Date | null;
+    _completedDate: Date | null;
+    _userTags: string[];
 
-export interface PlanningIndexCard {
-    Name: string;
-    ModeTag: string;
-    IdentTag: string;
-    StatusTag: string;
-    Parent? : string;
-    TargetDate: Date | null;
-    AnticipatedDate: Date | null;
-    CompletedDate: Date | null;
-    UserTags: Array<string>;
-};
+    get name(): string;
+    get modeTag(): string;
+    get identTag(): string;
+    get statusTag(): string;
+    get targetDate(): Date | null;
+    get anticipatedDate(): Date | null;
+    get completedDate(): Date | null;
+    get userTags(): string[];
+
+    set name(value: string);
+    set modeTag(value: string);
+    set identTag(value: string);
+    set statusTag(value: string);
+    set targetDate(value: Date | null);
+    set anticipatedDate(value: Date | null);
+    set completedDate(value: Date | null);
+    set userTags(value: string[]);
+}
+export class PlanningIndexCard implements IIndexCard {
+    _name: string;
+    _modeTag: string;
+    _identTag: string;
+    _statusTag: string;
+    _targetDate: Date | null;
+    _anticipatedDate: Date | null;
+    _completedDate: Date | null;
+    _userTags: string[];
+
+    constructor(){
+        this._name = "";
+        this._modeTag = "";
+        this._identTag = "";
+        this._statusTag = "";
+        this._targetDate = null;
+        this._anticipatedDate = null;
+        this._completedDate = null;
+        this._userTags = [];
+        };
+
+    get name(): string {
+        return this._name;
+    };
+
+    get modeTag(): string {
+        return this._modeTag;
+    };
+
+    get identTag(): string {
+        return this._identTag;
+    };
+
+    get statusTag(): string {
+        return this._statusTag;
+    };
+
+    get targetDate(): Date | null {
+        return this._targetDate;
+    };
+
+    get anticipatedDate(): Date | null {
+        return this._anticipatedDate;
+    };
+
+    get completedDate(): Date | null {
+        return this._completedDate;
+    };
+
+    get userTags(): string[] {
+        return this._userTags;
+    };
+
+    set name(value: string) {
+        this._name = value;
+    };
+
+    set modeTag(value: string) {
+        if (value.startsWith("#planning/")) {
+            this._modeTag = value
+        } 
+        else {
+            throw new BadModeTagError();
+        }
+    };
+
+    set identTag(value: string) {
+        if (value.startsWith("#planning/")) {
+            this._identTag = value;
+        }
+        else {
+            throw new BadIdentTagError();
+        }
+    };
+
+    set statusTag(value: string) {
+        if (value.startsWith("#status/")) {
+            this._modeTag = value;
+        }
+        else {
+            throw new BadStatusTagError();
+        }
+    };
+
+    set targetDate(value: Date | null)
+    {
+        this.targetDate = value;
+    };
+
+    set anticipatedDate(value: Date | null) {
+        this._anticipatedDate = value;
+    };
+
+    set completedDate(value: Date | null) {
+        this._completedDate = value;
+    };
+
+    set userTags(value: string[]) {
+        value.forEach(element => {
+            if (! element.startsWith("#"))
+            {
+                throw new UserTagError();
+            }
+        });
+        this.userTags = arraycopy(value);
+    };
+}
