512e9dc9709ea60b7b19308cad8db4025dada4f8
Bobw147 <bobw147@gmail.com>
2024-11-13 22:10:37 +0000

    Added createFolder

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,7 @@
 // Credits go to SilentVoid13's Templater PlugIn: https://github.com/SilentVoid13/Templater
 
 import { json } from "stream/consumers";
+import { Vault } from "obsidian";
 
 export function arraymove<T>(
     arr: T[],
@@ -17,4 +18,17 @@ export function arraymove<T>(
 
 export function arraycopy<T>(source: T): T {
     return JSON.parse(JSON.stringify(source)) as typeof source
+}
+
+export async function createFolder(vault: Vault, path: string){
+    let folders: string[];
+    let vaultFolder: string = "";
+
+    folders = path.split('/');
+    folders.forEach(async folder => {
+        vaultFolder = (vaultFolder == "") ? folder : vaultFolder + '/' + folder;
+        if (vault.getFolderByPath(vaultFolder) == null){
+            await vault.createFolder(vaultFolder);
+        }
+    });
 }
