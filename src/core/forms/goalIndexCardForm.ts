8af3192087ed1b6cc51ae42ee39cd3c7a7727600
Bobw147 <bobw147@gmail.com>
2024-12-06 15:33:21 +0000

    Deprecated the locally built form to use new generic version

--- src/core/forms/goalIndexCardForm.ts
+++ src/core/forms/goalIndexCardForm.ts
@@ -10,213 +10,19 @@ import { UserMessageId } from "../i18n";
 import { ICreateICForm } from "../types/interfaces";
 import { AttribSettingsId } from "../webbuilder/AtrribSettingsTypes";
 import { Settings } from "src/settings/Settings";
+import { GenericPlanningForm } from "./genericPlanningForm";
 
-export enum DisplayMode {
-    INDEX_CARD_MODE,
-    CREATE_MODE,
-}
+export class CreateGoalForm extends GenericPlanningForm implements ICreateICForm {
 
-export class CreateGoalForm implements ICreateICForm {
-
-    buildForm(parent: HTMLElement): void
-    {
-        const nodeBuilder = new NodeBuilder();
-
-        // Put all of the form together regardless of how it is being used
-        try {
-/*          const containerDiv1 = nodeBuilder.createElement(HtmlTags.DIV, [
-                [attrib.ID, field.ID_IC_GOAL_INDEX_CARD],
-            ]).appendChild(nodeBuilder.createElement(HtmlTags.DIV) as HTMLDivElement)
-                .appendChild(
-                    nodeBuilder.createElement(HtmlTags.LABEL, [
-                        [attrib.ID, field.ID_IC_GOAL_NAME_LABEL],
-                        [attrib.INNERTEXT, UserMessageId.GOAL_NAME_LABEL],
-                    ])
-                ).parentElement?.appendChild(
-                    nodeBuilder.createElement(HtmlTags.INPUT, [
-                        [attrib.ID, field.ID_CF_GOAL_NAME]
-                    ])
-                );
-            if (containerDiv1 !== undefined)
-                parent?.appendChild(containerDiv1);
-*/
-            const containerDiv = nodeBuilder.createElement(HtmlTags.DIV, [
-                [attrib.ID, field.ID_IC_GOAL_INDEX_CARD],
-            ]);
-
-            /*==== Create the GoalName section ====*/
-            const goalNameContainerDiv = nodeBuilder.createElement(HtmlTags.DIV);
-            goalNameContainerDiv.appendChild(
-                nodeBuilder.createElement(HtmlTags.LABEL, [
-                    [attrib.ID, field.ID_IC_GOAL_NAME_LABEL],
-                    [attrib.INNERTEXT, UserMessageId.GOAL_NAME_LABEL],
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.INPUT, [
-                    [attrib.ID, field.ID_IC_GOAL_NAME],
-                    [attrib.TYPE, AttribSettingsId.TEXT]
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.I, [
-                    [attrib.ID, FormFieldId.ID_IC_GOAL_NAME_ICON],
-                    [attrib.ICON, AttribSettingsId.LOCK],
-                    [attrib.CLASS, FormFieldId.STYLE_DIV_HIDDEN],
-                ])
-            );
-            containerDiv?.appendChild(goalNameContainerDiv);
-
-            /*==== Create the CategoryTag section ====*/
-            const goalCategoryTagContainerDiv = nodeBuilder.createElement(HtmlTags.DIV, [
-                [attrib.ID, FormFieldId.ID_IC_GOAL_CATEGORY_TAG_SECTION],
-            ]);
-            goalCategoryTagContainerDiv.appendChild(
-                nodeBuilder.createElement(HtmlTags.LABEL, [
-                    [attrib.ID, field.ID_IC_GOAL_CATEGORY_TAG_LABEL],
-                    [attrib.INNERTEXT, UserMessageId.CATEGORY_TAG_LABEL],
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.SELECT, [
-                    [attrib.ID, field.ID_IC_GOAL_CATEGORY_TAG]
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.I, [
-                    [attrib.ID, FormFieldId.ID_IC_GOAL_CATEGORY_TAG_ICON],
-                    [attrib.ICON, AttribSettingsId.LOCK],
-                    [attrib.CLASS, FormFieldId.STYLE_DIV_HIDDEN],
-                ])
-            );
-            containerDiv?.appendChild(goalCategoryTagContainerDiv);
- 
-            /*==== Create the StatusTag section ====*/
-            const goalStatusTagContainerDiv = nodeBuilder.createElement(HtmlTags.DIV, [
-                [attrib.ID, FormFieldId.ID_IC_GOAL_STATUS_TAG_SECTION],
-            ]);
-            goalStatusTagContainerDiv.appendChild(
-                nodeBuilder.createElement(HtmlTags.LABEL, [
-                    [attrib.ID, field.ID_IC_GOAL_STATUS_TAG_LABEL],
-                    [attrib.INNERTEXT, UserMessageId.STATUS_TAG_LABEL],
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.SELECT, [
-                    [attrib.ID, field.ID_IC_GOAL_STATUS_TAG]
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.I, [
-                    [attrib.ID, field.ID_IC_GOAL_STATUS_TAG_ICON],
-                    [attrib.ICON, AttribSettingsId.LOCK],
-                    [attrib.CLASS, FormFieldId.STYLE_DIV_HIDDEN],
-                ])
-            );
-            containerDiv?.appendChild(goalStatusTagContainerDiv);
-
-            /*==== Create the TargetDate section ====*/
-            const goalTargetDateContainerDiv = nodeBuilder.createElement(HtmlTags.DIV);
-            goalTargetDateContainerDiv.appendChild(
-                nodeBuilder.createElement(HtmlTags.LABEL, [
-                    [attrib.ID, field.ID_IC_GOAL_TARGET_DATE_LABEL],
-                    [attrib.INNERTEXT, UserMessageId.TARGET_DATE_LABEL],
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.INPUT, [
-                    [attrib.ID, field.ID_IC_GOAL_TARGET_DATE],
-                    [attrib.TYPE, AttribSettingsId.DATE],
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.I, [
-                    [attrib.ID, field.ID_IC_GOAL_STATUS_TAG_ICON],
-                    [attrib.ICON, AttribSettingsId.LOCK],
-                    [attrib.CLASS, FormFieldId.STYLE_DIV_HIDDEN],
-                ])
-            );
-            containerDiv?.appendChild(goalTargetDateContainerDiv);
-
-            /*==== Create the ExpectedDate field fragment ====*/
-            const goalExpectedDateContainerDiv = nodeBuilder.createElement(HtmlTags.DIV, [
-                [attrib.ID, FormFieldId.ID_IC_GOAL_EXPECTED_DATE_SECTION],
-            ]);
-            goalExpectedDateContainerDiv.appendChild(
-                nodeBuilder.createElement(HtmlTags.LABEL, [
-                    [attrib.ID, field.ID_IC_GOAL_EXPECTED_DATE_LABEL],
-                    [attrib.INNERTEXT, UserMessageId.EXPECTED_DATE_LABEL],
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.INPUT, [
-                    [attrib.ID, field.ID_IC_GOAL_EXPECTED_DATE],
-                    [attrib.TYPE, AttribSettingsId.DATE],
-                ])
-            );
-            containerDiv?.appendChild(goalExpectedDateContainerDiv);
-
-            /*==== Create the CompletedDate field fragment ====*/
-            const goalCompletedDateContainerDiv = nodeBuilder.createElement(HtmlTags.DIV, [
-                [attrib.ID, FormFieldId.ID_IC_GOAL_COMPLETED_DATE_SECTION],
-            ]);
-            goalCompletedDateContainerDiv.appendChild(
-                nodeBuilder.createElement(HtmlTags.LABEL, [
-                    [attrib.ID, field.ID_IC_GOAL_COMPLETED_DATE_LABEL],
-                    [attrib.INNERTEXT, UserMessageId.COMPLETED_DATE_LABEL],
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.INPUT, [
-                 [attrib.ID, field.ID_IC_GOAL_COMPLETED_DATE],
-                 [attrib.TYPE, AttribSettingsId.DATE],
-                ])
-            );
-            containerDiv?.appendChild(goalCompletedDateContainerDiv);
-
-            /*==== Create UserTags field section ====*/
-            const goalUserTagsContainerDiv = nodeBuilder.createElement(HtmlTags.DIV);
-            goalUserTagsContainerDiv.appendChild(
-                nodeBuilder.createElement(HtmlTags.LABEL, [
-                    [attrib.INNERTEXT, UserMessageId.USER_TAGS_LABEL],
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.INPUT, [
-                    [attrib.ID, field.ID_IC_GOAL_USER_TAGS],
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.I, [
-                    [attrib.ID, field.ID_IC_GOAL_STATUS_TAG_ICON],
-                    [attrib.ICON, AttribSettingsId.LOCK],
-                    [attrib.CLASS, FormFieldId.STYLE_DIV_HIDDEN],
-                ])
-                
-            )
-            containerDiv?.appendChild(goalUserTagsContainerDiv);
-
-            /*==== Create the Buttons section ====*/
-            const goalButtonsContainerDiv = nodeBuilder.createElement(HtmlTags.DIV, [
-                [attrib.ID, field.ID_IC_GOAL_BUTTONS]
-            ]);
-            goalButtonsContainerDiv.appendChild(
-                nodeBuilder.createElement(HtmlTags.BUTTON, [
-                    [attrib.ID, field.ID_IC_GOAL_CREATE_BUTTON],
-                    [attrib.INNERTEXT, UserMessageId.GOAL_CREATE_BUTTON_TEXT],
-                ])
-            ).parentElement?.appendChild(
-                nodeBuilder.createElement(HtmlTags.BUTTON, [
-                    [attrib.ID, field.ID_IC_GOAL_CREATE_BUTTON],
-                    [attrib.INNERTEXT, UserMessageId.CANCEL_BUTTON_TEXT],
-                ])
-            );
-            containerDiv?.appendChild(goalButtonsContainerDiv);
-
-            if (containerDiv !== undefined) {
-            parent.appendChild(containerDiv);
-            }
-        }
-        catch (DOMNodeBuildError) {
-            //TODO Determine what to do here
-            alert("DOMNodeBuildError Exception");
-            return;
-        }
+    buildForm(parent: HTMLElement): void {
+        super.buildForm(parent);
     }
-    
+
     configureForCreateMode(settings: Settings): void {
         const nodeBuilder: NodeBuilder = new NodeBuilder();
 
         // Add the CategoryTag options
-        const goalCategoryTag: HTMLSelectElement = document.getElementById(resolveField(FormFieldId.ID_IC_GOAL_CATEGORY_TAG, WrapperType.NONE)) as HTMLSelectElement
+        const goalCategoryTag: HTMLSelectElement = document.getElementById(resolveField(FormFieldId.GF_CATEGORY_TAG, WrapperType.NONE)) as HTMLSelectElement
         settings.categoryTags.forEach(
             (tag: string) => {
                 const element = nodeBuilder.createElement(HtmlTags.OPTION, [
@@ -227,13 +33,13 @@ export class CreateGoalForm implements ICreateICForm {
         )
 
         // Hide the Status Tag section
-        const statusTagsDiv = document.getElementById(resolveField(FormFieldId.ID_IC_GOAL_STATUS_TAG_SECTION, WrapperType.NONE));
+        const statusTagsDiv = document.getElementById(resolveField(FormFieldId.GF_STATUS_TAG_SECTION, WrapperType.NONE));
         nodeBuilder.setAttributes(statusTagsDiv, [[HtmlAttributes.CLASS, FormFieldId.STYLE_DIV_HIDDEN]]);
 
-        const expectedDateSectionDiv = document.getElementById(resolveField(FormFieldId.ID_IC_GOAL_EXPECTED_DATE_SECTION, WrapperType.NONE));
+        const expectedDateSectionDiv = document.getElementById(resolveField(FormFieldId.GF_EXPECTED_DATE_SECTION, WrapperType.NONE));
         nodeBuilder.setAttributes(expectedDateSectionDiv, [[HtmlAttributes.CLASS, FormFieldId.STYLE_DIV_HIDDEN]]);
 
-        const completedDateSectionDiv = document.getElementById(resolveField(FormFieldId.ID_IC_GOAL_COMPLETED_DATE_SECTION, WrapperType.NONE));
+        const completedDateSectionDiv = document.getElementById(resolveField(FormFieldId.GF_COMPLETED_DATE_SECTION, WrapperType.NONE));
         nodeBuilder.setAttributes(completedDateSectionDiv, [[HtmlAttributes.CLASS, FormFieldId.STYLE_DIV_HIDDEN]]);
     }
     
@@ -242,7 +48,7 @@ export class CreateGoalForm implements ICreateICForm {
         const goalIndexCard = new GoalIndexCard();
  
         // Add the CategoryTag options
-        const goalCategoryTag: HTMLSelectElement = document.getElementById(resolveField(FormFieldId.ID_IC_GOAL_CATEGORY_TAG, WrapperType.NONE)) as HTMLSelectElement
+        const goalCategoryTag: HTMLSelectElement = document.getElementById(resolveField(FormFieldId.GF_CATEGORY_TAG, WrapperType.NONE)) as HTMLSelectElement
         settings.categoryTags.forEach(
             (tag: string) => {
                 const element = nodeBuilder.createElement(HtmlTags.OPTION, [
@@ -253,7 +59,7 @@ export class CreateGoalForm implements ICreateICForm {
         )
 
         // Add the StatusTag options
-        const goalStatusTag: HTMLSelectElement = document.getElementById(resolveField(FormFieldId.ID_IC_GOAL_STATUS_TAG, WrapperType.NONE)) as HTMLSelectElement
+        const goalStatusTag: HTMLSelectElement = document.getElementById(resolveField(FormFieldId.GF_STATUS_TAG, WrapperType.NONE)) as HTMLSelectElement
         settings.statusTags.forEach(
             (tag: string) => {
                 const element = nodeBuilder.createElement(HtmlTags.OPTION, [
@@ -264,7 +70,7 @@ export class CreateGoalForm implements ICreateICForm {
         )
 
         // Hide the Create and Cancel buttons
-        nodeBuilder.setElementAttributes(FormFieldId.ID_IC_GOAL_BUTTONS, [[HtmlAttributes.CLASS,  FormFieldId.STYLE_DIV_HIDDEN]]);
+        nodeBuilder.setElementAttributes(FormFieldId.GF_BUTTONS, [[HtmlAttributes.CLASS,  FormFieldId.STYLE_DIV_HIDDEN]]);
 
         // Populate the goal index card
         await fileManager.processFrontMatter(file, (frontmatter) => {
@@ -272,31 +78,31 @@ export class CreateGoalForm implements ICreateICForm {
         });
 
         // Populate the form fields and make them read-only
-        nodeBuilder.setElementAttributes(field.ID_IC_GOAL_NAME, [
+        nodeBuilder.setElementAttributes(field.GF_NAME, [
             [HtmlAttributes.VALUE, goalIndexCard.name],
             [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
         ]);
-        nodeBuilder.setElementAttributes(field.ID_IC_GOAL_CATEGORY_TAG, [
+        nodeBuilder.setElementAttributes(field.GF_CATEGORY_TAG, [
             [HtmlAttributes.VALUE, goalIndexCard.categoryTag],
             [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
         ]);
-        nodeBuilder.setElementAttributes(field.ID_IC_GOAL_STATUS_TAG, [
+        nodeBuilder.setElementAttributes(field.GF_STATUS_TAG, [
             [HtmlAttributes.VALUE, goalIndexCard.statusTag],
             [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
         ]);
-        nodeBuilder.setElementAttributes(field.ID_IC_GOAL_TARGET_DATE, [
+        nodeBuilder.setElementAttributes(field.GF_TARGET_DATE, [
             [HtmlAttributes.VALUE, (goalIndexCard.targetDate != null) ? dateFormatter(goalIndexCard.targetDate) : emptyString],
             [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
         ]);
-        nodeBuilder.setElementAttributes(field.ID_IC_GOAL_EXPECTED_DATE, [
+        nodeBuilder.setElementAttributes(field.GF_EXPECTED_DATE, [
             [HtmlAttributes.VALUE, (goalIndexCard.expectedDate != null) ? dateFormatter(goalIndexCard.expectedDate) : emptyString],
             [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
         ]);
-        nodeBuilder.setElementAttributes(field.ID_IC_GOAL_COMPLETED_DATE, [
+        nodeBuilder.setElementAttributes(field.GF_COMPLETED_DATE, [
             [HtmlAttributes.VALUE, (goalIndexCard.completedDate != null) ? dateFormatter(goalIndexCard.completedDate) : emptyString],
             [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
         ]);
-        nodeBuilder.setElementAttributes(field.ID_IC_GOAL_USER_TAGS, [
+        nodeBuilder.setElementAttributes(field.GF_USER_TAGS, [
             [HtmlAttributes.VALUE, flattenedTags(goalIndexCard.userTags)],
             [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
         ]);
