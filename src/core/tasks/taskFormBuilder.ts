cb987e6bb680601deb10d0d6fdc3b0f0a4862c84
Bobw147 <bobw147@gmail.com>
2024-12-08 23:51:34 +0000

    Implemen IPlanningForm

--- src/core/tasks/taskFormBuilder.ts
+++ src/core/tasks/taskFormBuilder.ts
@@ -3,60 +3,97 @@ import { FormFieldId, resolveField } from "../formbuilder/formFieldTypes";
 import { dateFormatter, flattenedTags } from "src/utils/utils";
 import { TaskIndexCard } from "./taskIndexCard";
 import { emptyString, WrapperType } from "../types/types";
+import { GenericPlanningForm, IPlanningForm } from "../baseclasses/genericPlanningForm";
+import { Settings } from "src/settings/Settings";
+import { NodeBuilder } from "../formbuilder/nodebuilder";
+import { assignTagOptions } from "src/utils/utils";
+import { HtmlAttributes } from "../formbuilder/htmlAttributeTypes";
+import { HtmlTags } from "../formbuilder/htmlElementTypes";
+import { FormFieldId as field} from "../formbuilder/formFieldTypes";
+import { DisplayMode } from "../baseclasses/genericPlanningForm";
+import { AttribSettingsId } from "../formbuilder/AtrribSettingsTypes";
 
-const _taskIndexCardForm = '<div id='+resolveField(FormFieldId.ID_IC_TASK_INDEX_CARD, WrapperType.SINGLE_QUOTE)+' style="border-width: 2px; border-style: solid; border-color: gray;"> \
-    <div> \
-        <label style="margin-inline: 12px">Project :</label> \
-        <input type="text" id='+resolveField(FormFieldId.ID_IC_TASK_NAME, WrapperType.SINGLE_QUOTE)+' readonly style="margin-bottom: 12px"> \
-    </div> \
-    <div> \
-        <label style="margin-inline: 12px">Category :</label> \
-        <input type="text" id='+resolveField(FormFieldId.ID_IC_TASK_CATEGORY_TAG, WrapperType.SINGLE_QUOTE)+' readonly style="margin-bottom: 12px"> \
-   </div> \
-    <div style="display:none"> \
-        <label style="margin-inline: 12px">Ident :</label> \
-        <input type="text" id='+resolveField(FormFieldId.ID_IC_TASK_IDENT_TAG, WrapperType.SINGLE_QUOTE)+' readonly style="margin-bottom: 12px"> \
-   </div> \
-    <div> \
-        <label style="margin-inline: 12px">Status :</label> \
-        <input type="text" id='+resolveField(FormFieldId.ID_IC_TASK_STATUS_TAG, WrapperType.SINGLE_QUOTE)+' readonly style="margin-bottom: 12px"> \
-   </div> \
-    <div> \
-        <label style="margin-inline: 12px">Target Date</label> \
-        <input type="date" id='+resolveField(FormFieldId.ID_IC_TASK_TARGET_DATE, WrapperType.SINGLE_QUOTE)+' readonly style="margin-bottom: 12px"> \
-    </div> \
-    <div> \
-        <label style="margin-inline: 12px">Anticipated Date</label> \
-        <input type="date" id='+resolveField(FormFieldId.ID_IC_TASK_EXPECTED_DATE, WrapperType.SINGLE_QUOTE)+' readonly style="margin-bottom: 12px"> \
-    </div> \
-    <div> \
-        <label style="margin-inline: 12px">Completed Date</label> \
-        <input type="date" id='+resolveField(FormFieldId.ID_IC_TASK_COMPLETED_DATE, WrapperType.SINGLE_QUOTE)+' readonly style="margin-bottom: 12px"> \
-    </div> \
-    <div> \
-        <label style="margin-inline: 12px">User Tags</label> \
-        <input type="input" id='+resolveField(FormFieldId.ID_IC_TASK_USER_TAGS, WrapperType.SINGLE_QUOTE)+' readonly style="margin-bottom: 12px"> \
-    </div> \
-<div>'
+export class TaskFormBuilder extends GenericPlanningForm implements IPlanningForm {
 
-export function taskIndexCardForm() : string {
-    return _taskIndexCardForm;
-}
+    buildForm(parent: HTMLElement): void {
+        super.buildForm(parent);
+    }
 
-export async function populateTaskIndexCardForm(fileManager: FileManager, file: TFile): Promise<void> {
-    const projectIndexCard = new TaskIndexCard();
+    configureForCreateMode(settings: Settings): void {
+        const nodeBuilder: NodeBuilder = new NodeBuilder();
 
-    // Populate the project index card
-    await fileManager.processFrontMatter(file, (frontmatter) => {
-        projectIndexCard.load(frontmatter, file);
-    });
-    // Populate the form fields
-    (document.getElementById(resolveField(FormFieldId.ID_IC_TASK_NAME, WrapperType.NONE)) as HTMLInputElement).value = projectIndexCard.name;
-    (document.getElementById(resolveField(FormFieldId.ID_IC_TASK_CATEGORY_TAG, WrapperType.NONE)) as HTMLInputElement).value = projectIndexCard.categoryTag;
-    (document.getElementById(resolveField(FormFieldId.ID_IC_TASK_IDENT_TAG, WrapperType.NONE)) as HTMLInputElement).value = projectIndexCard.identTag;
-    (document.getElementById(resolveField(FormFieldId.ID_IC_TASK_STATUS_TAG, WrapperType.NONE)) as HTMLInputElement).value = projectIndexCard.statusTag;
-    (document.getElementById(resolveField(FormFieldId.ID_IC_TASK_TARGET_DATE, WrapperType.NONE)) as HTMLInputElement).value = (projectIndexCard.targetDate != null) ? dateFormatter(projectIndexCard.targetDate) : emptyString;
-    (document.getElementById(resolveField(FormFieldId.ID_IC_TASK_EXPECTED_DATE, WrapperType.NONE)) as HTMLInputElement).value = (projectIndexCard.expectedDate != null) ? dateFormatter(projectIndexCard.expectedDate) : emptyString;
-    (document.getElementById(resolveField(FormFieldId.ID_IC_TASK_COMPLETED_DATE, WrapperType.NONE)) as HTMLInputElement).value = (projectIndexCard.completedDate != null) ? dateFormatter(projectIndexCard.completedDate) : emptyString;
-    (document.getElementById(resolveField(FormFieldId.ID_IC_TASK_USER_TAGS, WrapperType.NONE)) as HTMLInputElement).value = flattenedTags(projectIndexCard.userTags);
+        // Populate the Category & Status selectors with options the list contained in the plugin settings
+        const categorySelect = document.getElementById(resolveField(FormFieldId.GF_CATEGORY_TAG, WrapperType.NONE)) as HTMLSelectElement;           
+        assignTagOptions(categorySelect, settings.categoryTags)
+
+        // Hide the unused sections
+        const memberOfDiv = document.getElementById(resolveField(FormFieldId.GF_MEMBER_OF_SECTION, WrapperType.NONE));
+        nodeBuilder.setAttributes(memberOfDiv, [[HtmlAttributes.CLASS, FormFieldId.STYLE_DIV_HIDDEN]]);
+
+        const expectedDateSectionDiv = document.getElementById(resolveField(FormFieldId.GF_EXPECTED_DATE_SECTION, WrapperType.NONE));
+        nodeBuilder.setAttributes(expectedDateSectionDiv, [[HtmlAttributes.CLASS, FormFieldId.STYLE_DIV_HIDDEN]]);
+
+        const completedDateSectionDiv = document.getElementById(resolveField(FormFieldId.GF_COMPLETED_DATE_SECTION, WrapperType.NONE));
+        nodeBuilder.setAttributes(completedDateSectionDiv, [[HtmlAttributes.CLASS, FormFieldId.STYLE_DIV_HIDDEN]]);
+    }
+
+    async configureForIndexCardMode(settings: Settings, fileManager: FileManager, file: TFile): Promise<void> {
+        const nodeBuilder: NodeBuilder = new NodeBuilder();
+        const taskIndexCard = new TaskIndexCard();
+
+        // Add the CategoryTag options
+        const taskCategorySelect: HTMLSelectElement = document.getElementById(resolveField(FormFieldId.GF_CATEGORY_TAG, WrapperType.NONE)) as HTMLSelectElement
+        assignTagOptions(taskCategorySelect, settings.categoryTags);
+    
+        // Add the StatusTag options
+        const taskStatusSelect: HTMLSelectElement = document.getElementById(resolveField(FormFieldId.GF_STATUS_TAG, WrapperType.NONE)) as HTMLSelectElement
+        assignTagOptions(taskStatusSelect, settings.statusTags);
+
+        // Hide the Create and Cancel buttons
+        nodeBuilder.setElementAttributes(FormFieldId.GF_BUTTONS, [[HtmlAttributes.CLASS, FormFieldId.STYLE_DIV_HIDDEN]]);
+
+        // Populate the task index card
+        await fileManager.processFrontMatter(file, (frontmatter) => {
+            taskIndexCard.load(frontmatter);
+        });
+
+        // Populate the form fields and make them read-only
+        nodeBuilder.setElementAttributes(field.GF_NAME, [
+            [HtmlAttributes.VALUE, taskIndexCard.name],
+            [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
+        ]);
+        nodeBuilder.setElementAttributes(field.GF_CATEGORY_TAG, [
+            [HtmlAttributes.VALUE, taskIndexCard.categoryTag],
+            [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
+        ]);
+        nodeBuilder.setElementAttributes(field.GF_STATUS_TAG, [
+            [HtmlAttributes.VALUE, taskIndexCard.statusTag],
+            [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
+        ]);
+        nodeBuilder.setElementAttributes(field.GF_TARGET_DATE, [
+            [HtmlAttributes.VALUE, (taskIndexCard.targetDate != null) ? dateFormatter(taskIndexCard.targetDate) : emptyString],
+            [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
+        ]);
+        nodeBuilder.setElementAttributes(field.GF_EXPECTED_DATE, [
+            [HtmlAttributes.VALUE, (taskIndexCard.expectedDate != null) ? dateFormatter(taskIndexCard.expectedDate) : emptyString],
+            [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
+        ]);
+        nodeBuilder.setElementAttributes(field.GF_COMPLETED_DATE, [
+            [HtmlAttributes.VALUE, (taskIndexCard.completedDate != null) ? dateFormatter(taskIndexCard.completedDate) : emptyString],
+            [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
+        ]);
+        nodeBuilder.setElementAttributes(field.GF_USER_TAGS, [
+            [HtmlAttributes.VALUE, flattenedTags(taskIndexCard.userTags)],
+            [HtmlAttributes.DISABLED, AttribSettingsId.TRUE],
+        ]);
+    }
+
+    updateIndexCard(taskIndexCard: TaskIndexCard, displayMode: DisplayMode): void {
+        taskIndexCard.name = NodeBuilder.getElementInfo(HtmlTags.INPUT, FormFieldId.GF_NAME, HtmlAttributes.VALUE);
+        taskIndexCard.categoryTag = NodeBuilder.getElementInfo(HtmlTags.SELECT, FormFieldId.GF_CATEGORY_TAG, HtmlAttributes.VALUE);
+        taskIndexCard.targetDate = new Date(NodeBuilder.getElementInfo(HtmlTags.INPUT, FormFieldId.GF_TARGET_DATE, HtmlAttributes.VALUE));
+        if (displayMode == DisplayMode.INDEX_CARD_MODE) {
+
+        }
+    }
 }
