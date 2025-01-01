b9acca6737b9e99a3d5d7a3ce78e6dd597966677
Bobw147 <bobw147@gmail.com>
2025-01-01 14:40:25 +0000

    Cancel button now submits hasChanged = false

--- src/core/goals/goals-modal.ts
+++ src/core/goals/goals-modal.ts
@@ -64,7 +64,7 @@ export class GoalsModal extends PlanningModal implements IModalForm {
                 
                 //  Add a handler to the 'Cancel' button
                 (this.buttonsSection.components[2] as ButtonComponent).onClick(async () => {
-                    this._onSubmit(true, this.app, this.settings);
+                    this._onSubmit(false, this.app, this.settings);
                 });
             }
         }
